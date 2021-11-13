30e9ba263a87df192a76d72db9458ca18d0294ef
Eric Correia <correia.eh@gmail.com>
2021-11-12 23:51:55 -0500

    create map and player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+mod map;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::player::*;
+}
+
+use prelude::*;
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        // ctx.print(1,1 ,"Hello, Bracket Terminal!");
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawl")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
 }
