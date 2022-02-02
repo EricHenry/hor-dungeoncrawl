306483cf2316a6685bf01311cee409ffaf83842e
Eric Correia <correia.eh@gmail.com>
2022-02-01 20:09:55 -0500

    fix tooltip placement on screen

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -11,14 +11,18 @@ pub fn tooltips(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camer
     let mut positions = <(Entity, &Point, &Name)>::query();
 
     let offset = Point::new(camera.left_x, camera.top_y);
-    let map_pos = *mouse_pos + offset; // screen position relative to the camera
+    let map_pos = *mouse_pos + offset; // screen position of an entity relative to the map
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
     positions
         .iter(ecs)
         .filter(|(_, pos, _)| **pos == map_pos)
         .for_each(|(entity, _, name)| {
-            let screen_pos = *mouse_pos * 4;
+            let screen_pos = *mouse_pos * 2;
+            println!(
+                "mouse pos: {:?}, screen pos: {:?}, map_pos: {:?}",
+                mouse_pos, screen_pos, map_pos
+            );
             let display =
                 if let Ok(health) = ecs.entry_ref(*entity).unwrap().get_component::<Health>() {
                     format!("{} : {} hp", &name.0, health.current)
