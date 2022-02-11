8126c786bc12cd748db6c2524cef49be8fb6598a
Eric Correia <correia.eh@gmail.com>
2022-02-11 07:41:41 -0500

    implement a memory feature to still render parts of the map the player has seen

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -13,13 +13,22 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(x, y);
+            let idx = map_idx(x, y);
+            if map.in_bounds(pt)
+                && (player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx])
+            {
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    WHITE
+                } else {
+                    DARK_GRAY
+                };
+                let color = ColorPair::new(tint, BLACK);
                 let glyph = match map.tiles[idx] {
                     TileType::Wall => to_cp437('.'),
                     TileType::Floor => to_cp437('#'),
                 };
-                draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), glyph);
+
+                draw_batch.set(pt - offset, color, glyph);
             }
         }
     }
